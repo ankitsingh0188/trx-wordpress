1417b3c7674002a2588cf0c1154907d797ad1294
Ankit Singh <ankitsingh0188@gmail.com>
2016-12-21 15:10:39 +0530

    Change the branch name

--- wp-content/themes/twentysixteen/jurisdiction.php
+++ wp-content/themes/twentysixteen/jurisdiction.php
@@ -12,6 +12,7 @@
  * @subpackage Twenty_Sixteen
  * @since Twenty Sixteen 1.0
  */
+
 get_header('trx-header'); ?>
 <div id="primary" class="content-area">
 	<main id="main" class="site-main" role="main">
